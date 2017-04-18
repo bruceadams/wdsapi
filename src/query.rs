c753bdc0148c85926f653f60241ca83a004c38a7
Bruce Adams <bruce.adams@acm.org>
2017-04-18 15:39:13 -0400

    Add query::notices

--- src/query.rs
+++ src/query.rs
@@ -11,3 +11,13 @@ pub fn query(creds: &Credentials,
                collection_id + "/query";
     Ok(discovery_api(creds, Get, &path, Query::Query(query), &Body::None)?)
 }
+
+pub fn notices(creds: &Credentials,
+               env_id: &str,
+               collection_id: &str,
+               query: QueryParams)
+               -> Result<Value, ApiError> {
+    let path = "/v1/environments/".to_string() + env_id + "/collections/" +
+               collection_id + "/notices";
+    Ok(discovery_api(creds, Get, &path, Query::Query(query), &Body::None)?)
+}
