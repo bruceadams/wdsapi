3ae480404b6c77420a778a6803338254da60a3ab
Bruce Adams <bruce.adams@acm.org>
2017-04-19 09:44:18 -0400

    Add support for user specified document_id

--- src/document.rs
+++ src/document.rs
@@ -17,10 +17,19 @@ pub fn create(creds: &Credentials,
               env_id: &str,
               collection_id: &str,
               configuration_id: Option<&str>,
+              document_id: Option<&str>,
               filename: &str)
               -> Result<Value, ApiError> {
-    let path = "/v1/environments/".to_string() + env_id + "/collections/" +
-               collection_id + "/documents";
+    let path = match document_id {
+        Some(id) => {
+            "/v1/environments/".to_string() + env_id + "/collections/" +
+            collection_id + "/documents/" + id
+        }
+        None => {
+            "/v1/environments/".to_string() + env_id + "/collections/" +
+            collection_id + "/documents"
+        }
+    };
     let q = match configuration_id {
         Some(id) => Query::Config(id.to_string()),
         None => Query::None,
